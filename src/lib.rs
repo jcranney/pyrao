34f80d0206f868e0cadc00c0b48879e3da492d50
Jesse Cranney <jesse.cranney@anu.edu.au>
2024-08-20 19:21:49 -1000

    toy functions lingering

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,9 @@
 use pyo3::prelude::*;
 use rao::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-#[pyfunction]
-fn example_func_rust() -> PyResult<()> {
-    println!("this is a rust function!!");
-    Ok(())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyrao(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(example_func_rust, m)?)?;
     m.add_function(wrap_pyfunction!(ultimatestart_system_matrices, m)?)?;
     m.add_function(wrap_pyfunction!(ultimatestart_recon_matrices, m)?)?;
     m.add_class::<SystemMatrices>()?;
